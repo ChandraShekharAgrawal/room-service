bc18732d372c9e6d3afafc15154faaa6064524d2
ChandraShekharAgrawal <csa.jpr08@gmail.com>
2024-04-03 22:48:53 +0530

    created entities for service

---
+++ src/main/java/com/chandrashekhar/roomservice/entities/Room.java
@@ -0,0 +1,25 @@
+package com.chandrashekhar.roomservice.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "rooms")
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String roomNumber;
+
+    @ManyToOne
+    private RoomType roomType;
+}
---
+++ src/main/java/com/chandrashekhar/roomservice/entities/RoomType.java
@@ -0,0 +1,21 @@
+package com.chandrashekhar.roomservice.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class RoomType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private int capacity;
+    private int price;
+}
